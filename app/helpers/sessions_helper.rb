217f320cc75130e97471ef64cc371f6bf592011c
Chris Peters <christopher.peters@calignostics.net>
2014-01-14 17:16:12 -0800

    Stopping Point 8.2.4

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
+
+  def sign_in(user)
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = { value:    remember_token,
+    									   expires:  1.week.from_now.utc }
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+    self.current_user = user
+  end
+  
+  def current_user=(user)
+  	remember_token = User.encrypt(cookies[:remember_token])
+  	@current_user ||= User.find_by(remember_token: remember_token)
+  end
+
 end
